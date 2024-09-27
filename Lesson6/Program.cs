ace1989c32ad945cbe6a23badec8a0d68089ee2b
Sergey Khromenkov <khromenkov00@bk.ru>
2024-09-27 17:33:41 +0200

    ВВел правки

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,15 +1,15 @@
 ﻿try
 {
-    int DaysPassed, WeeksFull;
+    int a, b;
     Console.WriteLine("Введите, сколько прошло дней:");
-    DaysPassed = Convert.ToInt32(Console.ReadLine());
-    if (DaysPassed < 7)
-        Console.WriteLine($"С момента отсчета прошло 0 целых недель, а остаток дней равен - {DaysPassed} ");
-    if (DaysPassed <= -1)
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a < 7)
+        Console.WriteLine($"С момента отсчета прошло 0 целых недель, а остаток дней равен - {a} ");
+    if (a <= -1)
         Console.WriteLine("Количество дней не может быть меньше нуля ");
-    WeeksFull = DaysPassed / 7;
-    DaysPassed -= WeeksFull * 7;
-    Console.WriteLine($"Полных недель с начала отчета прошло - {WeeksFull}, остаток дней равен - {DaysPassed} ");
+    b = a / 7;
+    a -= b * 7;
+    Console.WriteLine($"Полных недель с начала отчета прошло - {b}, остаток дней равен - {a} ");
 }
 catch
 {
