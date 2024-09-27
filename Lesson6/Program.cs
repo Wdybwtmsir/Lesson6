43a04ddc1264e241f3c642faa3bfecc832fdbef5
Nill Kiggers <Nill Kiggers@209-10>
2024-09-27 17:23:56 +0200

    Добавьте файлы проекта.

---
+++ Lesson6/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    int DaysPassed, WeeksFull;
+    Console.WriteLine("Введите, сколько прошло дней:");
+    DaysPassed = Convert.ToInt32(Console.ReadLine());
+    if (DaysPassed < 7)
+        Console.WriteLine($"С момента отсчета прошло 0 целых недель, а остаток дней равен - {DaysPassed} ");
+    if (DaysPassed <= -1)
+        Console.WriteLine("Количество дней не может быть меньше нуля ");
+    WeeksFull = DaysPassed / 7;
+    DaysPassed -= WeeksFull * 7;
+    Console.WriteLine($"Полных недель с начала отчета прошло - {WeeksFull}, остаток дней равен - {DaysPassed} ");
+}
+catch
+{
+    Console.WriteLine("Введите правильные данные!");
+}
