9ecd3525e1c012953cb71c664c36c847edf91262
Sergey Khromenkov <khromenkov00@bk.ru>
2024-09-27 17:44:45 +0200

    ф

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -2,11 +2,7 @@
 {
     int a, b;
     Console.WriteLine("Введите, сколько прошло дней:");
-    a = Convert.ToInt32(Console.ReadLine());
-    if (a < 7)
-        Console.WriteLine($"С момента отсчета прошло 0 целых недель, а остаток дней равен - {a} ");
-    if (a <= -1)
-        Console.WriteLine("Количество дней не может быть меньше нуля ");
+    a = Convert.ToInt32(Console.ReadLine());    
     b = a / 7;
     a -= b * 7;
     Console.WriteLine($"Полных недель с начала отчета прошло - {b}, остаток дней равен - {a} ");
